f13d08a50c33f238370ebbd8e69812869f4ad83c
Riasalit <daniel-vilslev@hotmail.com>
2022-10-21 10:21:18 +0200

    fix

---
+++ constants.c
@@ -0,0 +1,3 @@
+#include <stdint.h>
+VALUE_SIZE_IN_BYTES = (uint8_t) sizeof(float);
+VALUE_SIZE_IN_BITS = (uint8_t) 8 * (uint8_t) sizeof(float);
--- constants.h
+++ constants.h
@@ -2,7 +2,7 @@
 #define CONSTS
 #include <stdint.h>
 
-const uint8_t VALUE_SIZE_IN_BYTES = (uint8_t) sizeof(float);
-const uint8_t VALUE_SIZE_IN_BITS = (uint8_t) 8 * (uint8_t) sizeof(float);
+const uint8_t VALUE_SIZE_IN_BYTES;
+const uint8_t VALUE_SIZE_IN_BITS;
 
 #endif
