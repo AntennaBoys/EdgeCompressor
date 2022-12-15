c6ab1c20f855679d71103ec13ecf7e6bc5e3660d
Riasalit <daniel-vilslev@hotmail.com>
2022-12-15 12:53:45 +0100

    fixed floats

--- jsonprint.c
+++ jsonprint.c
@@ -16,20 +16,32 @@ void closeFile(FILE* file){
     fclose(file);
 }
 
+int float_to_int(float val){
+    return *(int *)&val;
+}
+
+void dump(FILE* file, const void *data, size_t len) {
+    const unsigned char *x = data;
+    for (size_t k = len-1;k < len; k--){
+        fprintf(file, "%02x", x[k]);
+    }
+    fprintf(file,",");
+}
+
 void writeModelToFile(FILE* file, Timestamps timestamps, Selected_model model, int* first, int start_time, int end_time, double error, int column_id){
 
-    fprintf(file,"%d,", model.model_type_id);                   //Model id
-    fprintf(file,"%d,", column_id);                             //Column id
-    fprintf(file,"%d,", (int)model.end_index);                  //End index
-    fprintf(file,"%f,", model.min_value);                       //Min value
-    fprintf(file,"%f,", model.max_value);                       //Max value
-    fprintf(file,"%lf,", error);                                //Model error
+    fprintf(file,"%x,", model.model_type_id);                   //Model id
+    fprintf(file,"%x,", column_id);                             //Column id
+    fprintf(file,"%x,", (int)model.end_index);                  //End index
+    dump(file, &model.min_value, sizeof(float));                //Min value
+    dump(file, &model.max_value, sizeof(float));                //Max value
+    dump(file, &error, sizeof(double));                         //Model error
     for (int i = 0; i < model.values_capacity; i++){
         fprintf(file, "%02x", model.values[i]);                 //Values (use of this array differs from model to model)
     }
     fprintf(file,",");
-    fprintf(file,"%d,", start_time);                            //First timestamp
-    fprintf(file,"%d,", end_time);                              //Last timestamp
+    fprintf(file,"%x,", start_time);                            //First timestamp
+    fprintf(file,"%x,", end_time);                              //Last timestamp
     for (int i = 0; i < timestamps.compressed_timestamp_count; i++) {
         fprintf(file, "%02x", timestamps.compressed_time[i]);   //Compressed timestamps
     }
@@ -38,14 +50,14 @@ void writeModelToFile(FILE* file, Timestamps timestamps, Selected_model model, i
 
 void write_text_to_file(FILE* file, int* first, int column_id, int count, char* string, long start_time, long end_time, Timestamps timestamps){
     fprintf(file,"5,");                                         //Model id
-    fprintf(file,"%d,", column_id);                             //Column id
-    fprintf(file,"%d,", count-1);                               //End index
-    fprintf(file,"%d,", count);                                 //Min value (for text this is the running length of the string)
+    fprintf(file,"%x,", column_id);                             //Column id
+    fprintf(file,"%x,", count-1);                               //End index
+    fprintf(file,"%x,", count);                                 //Min value (for text this is the running length of the string)
     fprintf(file,",");                                          //Max value (not used for text)
     fprintf(file,"0,");                                         //Error (lossless compression, so always 0)
     fprintf(file,"%02x,", string);                              //Values (the string)
-    fprintf(file,"%d,", start_time);                            //First timestamp
-    fprintf(file,"%d,", end_time);                              //Last timestamp
+    fprintf(file,"%x,", start_time);                            //First timestamp
+    fprintf(file,"%x,", end_time);                              //Last timestamp
     for (int i = 0; i < timestamps.compressed_timestamp_count; i++){
         fprintf(file, "%02x", timestamps.compressed_time[i]);   //Compressed timestamps
     }
